c9f5e4c040b4ae389c09453fbcfb72d19b44de33
Jordan Ehrlich <jehrlich@harriscomputer.com>
2017-02-13 13:21:47 -0600

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -66,6 +66,39 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+	var body = _.pick(req.body, "completed", "description");
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+
+	if (!matchedTodo) {
+		return res.status(404).json({
+			"error": "no todo found with that id"
+		});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+	_.extend(matchedTodo, validAttributes);
+
+
+	res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '!');
 })
